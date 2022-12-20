64594a45d89ffd0c2a59251e6bcb9f1c0a398539
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-12-20 11:36:56 -0600

    edited client/src/js/database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,49 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+
+  // create a connection to the database and version
+  const contactDB = await openDb('jate', 1);
+
+  // Create a new transaction and specify the database & privileges
+  const tx = contactDB.transaction('jate', 'readwrite');
+
+  // Create a new object store
+  const store = tx.objectStore('jate');
+
+  // Add the content to the database
+  const request = store.put({ id: 1, value: content });
+
+  // Get confirmation of the request
+  const result = await request;
+  HTMLFormControlsCollection.log(
+    `🚀 Data saved to the the database: ${result}`,
+    result
+  );
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log(`GET from the database`);
+
+  // Create a connection to the database and version
+  const contactDB = await openDb('jate', 1);
+
+  // Create a new transaction and specify the database & privileges
+  const tx = contactDB.transaction('jate', 'readonly');
+
+  // Create a new object store
+  const store = tx.objectStore('jate');
+
+  // Get the content from the database
+  const request = store.getAll(1);
+
+  // Get confirmation of the request
+  const result = await request;
+  console.log(`🚀 Data retrieved from the database: ${result}`, result);
+  return result?.value;
+};
 
 initdb();
